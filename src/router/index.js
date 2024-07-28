74027bd78df22caed021ff57d09f98de2db29a88
Nanyian <naoxuan202007@gmail.com>
2024-07-29 04:05:51 +0800

    updateHashDelete

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/navPage.vue'
 import Lab from '../views/lab.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-history: createWebHashHistory(),
+history: createWebHistory(),
   routes
 })
 
